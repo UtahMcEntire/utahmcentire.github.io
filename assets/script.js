a17343186478d1deeb9051097fe599e586745155
Utah <46671092+UtahMcEntire@users.noreply.github.com>
2021-01-07 11:15:07 -0600

    SPP Update
    added spp landing page and sample out put, as well as some mobile updates to the homepage

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ function goBack() {
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
-	//document.cookie='sel_theme='+themeName+';';
+	document.cookie='sel_theme='+themeName+';';
 }
 // function to toggle between light and dark theme
 function toggleTheme() {
@@ -19,35 +19,15 @@ function toggleTheme() {
 }
 
 // Immediately invoked function to set the theme on initial load
-// If local time between 9pm and 6am, dark theme, else light theme
-var d = new Date();
-var currHour = d.getHours();
 (function () {
-	
-	/*var cookie_pos=document.cookie.indexOf('sel_theme=');//locate value in cookie
-	
+	var cookie_pos=document.cookie.indexOf('sel_theme=');//locate value in cookie
 	if(cookie_pos!=-1){//if string was found
 		var cookie_flavor=substr(cookie_pos+10,document.cookie.indexOf(';',cookie_pos));//extract the value of the cookie
 		//then run your already existing change theme function using the extracted name
 		setTheme(cookie_flavor);
 	}
 	
-	else {*/
-		if (localStorage.getItem('theme') === 'theme-dark') {
-			if (currHour >= 21 && currHour <= 6) {
-				setTheme('theme-dark');
-			}
-		}
-		
-		else {
-			setTheme('theme-light');
-		}
-	//}
+	else {
+		setTheme('theme-light');
+	}
 })();
-
-function hidediv(){
-  document.getElementById('showhide').style.display ='none';
-}
-function showdiv(){
-  document.getElementById('showhide').style.display = 'inline-block';
-}
